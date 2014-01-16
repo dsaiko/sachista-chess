1e5f5396a51c005cfeef6955d24900e2b607e407
Dusan Saiko <dusan.saiko@gmail.com>
2014-01-17 00:54:40 +0100

    tuning version

--- updateversion.sh
+++ updateversion.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+VER=$( git describe )
+DATE=$( date "+%Y-%m-%d %H:%M" )
+
+cat > movegenerator/version.h <<EOF
+
+#ifndef SACHISTA_CHESS_VERSION_H
+#define SACHISTA_CHESS_VERSION_H
+
+#define IMPLEMENTATION_VERSION "${VER}"
+#define IMPLEMENTATION_DATE "${DATE}"
+
+#endif
+
+
+EOF
+
+
+
