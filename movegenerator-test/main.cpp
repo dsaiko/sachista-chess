676d7a7929d2756bcc32da7a7cec2f0ef2a0dab4
Dusan Saiko <dusan.saiko@gmail.com>
2014-01-17 01:29:04 +0100

    Adding platform diagnostic message

--- movegenerator-test/main.cpp
+++ movegenerator-test/main.cpp
@@ -1,5 +1,24 @@
 #include <CppUTest/CommandLineTestRunner.h>
 
+#include "stdio.h"
+#include "version.h"
+
 int main(int ac, char** av) {
+
+
+#if defined(__i386__)
+    char architecture[] = "x86";
+#elif defined(__x86_64__)
+    char architecture[] = "x64";
+#else
+    char architecture[] = "UnknownArchitecture";
+#endif
+
+   printf("Welcome to %s sachista-chess %s (%s)!\n",
+          architecture,
+          IMPLEMENTATION_VERSION,
+          IMPLEMENTATION_DATE
+   );
+
    return CommandLineTestRunner::RunAllTests(ac, av);
 }
